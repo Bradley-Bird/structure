02953ff82454980ec76e2a66d61420a5611d87b1
Bradley-Bird <Bradleybird2@icloud.com>
2022-05-20 12:55:01 -0700

    request link now taking to detail page.

--- src/views/Requests.jsx
+++ src/views/Requests.jsx
@@ -1,17 +1,30 @@
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, List, ListItem } from '@mui/material';
 import { useRequest } from '../hooks/requestsHooks';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 function Requests() {
   const { requests } = useRequest();
   return (
     <div>
       <CssBaseline />
-      {requests.map((request) => (
-        <p key={request.id}>{request.request}</p>
-      ))}
+      <List>
+        {requests.map((request) => (
+          <StyledLink to={`/requests/${request.id}`} key={request.id}>
+            <ListItem>{request.request}</ListItem>
+          </StyledLink>
+        ))}
+      </List>
     </div>
   );
 }
-
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  padding: 10px;
+  border: 2px solid black;
+  border-radius: 20px;
+  display: flex;
+`;
 export default Requests;
