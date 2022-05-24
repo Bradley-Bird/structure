696490904e60431ed3e58878ffc76733ba1a1e7e
Bradley-Bird <Bradleybird2@icloud.com>
2022-05-23 17:11:25 -0700

    copy now working

--- src/hooks/formHooks.js
+++ src/hooks/formHooks.js
@@ -1,20 +1,26 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import toast from 'react-hot-toast';
+import { RequestContext } from '../context/RequestContext';
+
 import { useRequest } from '../hooks/requestsHooks';
 
 const useFormHooks = () => {
   const { history } = useRequest();
   const [value, setValue] = useState('');
+  const { setCeForm, ceForm } = useContext(RequestContext);
   const { handleRequestSubmit } = useRequest();
 
   const handleChange = (event) => {
-    setValue(event.target.value);
+    if (!ceForm) {
+      setValue(event.target.value);
+    }
+    setCeForm(event.target.value);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
-      handleRequestSubmit(value);
+      handleRequestSubmit(ceForm || value);
     } catch (err) {
       toast.error(err);
       throw err;
